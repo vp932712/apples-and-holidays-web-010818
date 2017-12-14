65c6f0c7842091aba09e8e244ecf3066ef1a255b
Vaibhav Patel <vp932712@gmail.com>
2017-12-14 22:54:55 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -43,7 +43,16 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+final = Winter:
+  Christmas: Lights, Wreath
+  New Years: Party Hats
+Summer:
+  Fourth Of July: Fireworks, BBQ
+Fall:
+  Thanksgiving: Turkey
+Spring:
+  Memorial Day: BBQ
+final
 end
 
 def all_supplies_in_holidays(holiday_hash)
